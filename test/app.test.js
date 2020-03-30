91c79bdbf31993752700171210d3585a5617a1bd
Rimvydas Zilinskas <rimvydas.zilinskas@yahoo.com>
2020-03-30 19:27:25 +0200

    change data

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const app = require('../app');
 
 describe('App', () => {
-    it('should be hello', () => {
-        assert.equal(app(), 'hello');
+    it('should be 1', () => {
+        assert.equal(app(), 2);
     });
 });
